3c721d57d28eea7ac047cb6ed7a3c884cf60079d
e34652 <155132260+e34652@users.noreply.github.com>
2024-04-25 17:48:22 +0900

    interceptor

--- _sb0423/src/main/java/com/sky/_sb0423/config/MvcConfig.java
+++ _sb0423/src/main/java/com/sky/_sb0423/config/MvcConfig.java
@@ -1,13 +1,26 @@
 package com.sky._sb0423.config;
 
+import com.sky._sb0423.interceptor.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+//@EnableWebMvc << 이거 쓰면 스프링부트 작동 오류 생길 가능성 매우 높음
+public class MvcConfig implements WebMvcConfigurer { //인터셉터 등의 기능을 쓸때 필수
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControllers(ViewControllerRegistry registry) { // 처음 켜지면 main이라는 뷰로 가라
         registry.addViewController("/").setViewName("main");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoggerInterceptor())
+                .addPathPatterns("/register/**")
+                .excludePathPatterns("/practice/**");
+
     }
 }
