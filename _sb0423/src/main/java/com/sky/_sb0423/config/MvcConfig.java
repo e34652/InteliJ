9b77b3364f0830d3d512dd1704bd0496c64358e0
e34652 <155132260+e34652@users.noreply.github.com>
2024-04-29 20:44:51 +0900

    interceptor

--- _sb0423/src/main/java/com/sky/_sb0423/config/MvcConfig.java
+++ _sb0423/src/main/java/com/sky/_sb0423/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.sky._sb0423.config;
 
+import com.sky._sb0423.interceptor.AuthCheckInterceptor;
 import com.sky._sb0423.interceptor.LoggerInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,19 +10,23 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc << 이거 쓰면 스프링부트 작동 오류 생길 가능성 매우 높음
-public class MvcConfig implements WebMvcConfigurer { //인터셉터 등의 기능을 쓸때 필수
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+
+    private final AuthCheckInterceptor authCheckInterceptor;
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) { // 처음 켜지면 main이라는 뷰로 가라
+    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("main");
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
         registry.addInterceptor(new LoggerInterceptor())
                 .addPathPatterns("/register/**")
                 .excludePathPatterns("/practice/**");
+
+        registry.addInterceptor(authCheckInterceptor)
+                .addPathPatterns("/edit/**");
 
     }
 }
