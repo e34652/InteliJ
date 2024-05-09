d7ac86b70e0698c4396d1d870c1857d87b19e9ef
e34652 <155132260+e34652@users.noreply.github.com>
2024-05-09 20:58:55 +0900

    test

---
+++ kube0508/src/test/java/com/team1/kube0508/Kube0508ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.kube0508;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kube0508ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
